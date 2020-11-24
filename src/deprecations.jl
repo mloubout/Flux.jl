9c6d59d5ed0d969009ab45cc68369016547eb31b
Carlo Lucibello <carlo.lucibello@gmail.com>
2020-11-24 09:46:02 +0100

    add deprecations

--- src/deprecations.jl
+++ src/deprecations.jl
@@ -3,3 +3,12 @@
 @deprecate InstanceNorm(λ, β, γ, μ, σ², ϵ, momentum) InstanceNorm(λ, β, γ, μ, σ², ϵ, momentum, nothing)
 @deprecate BatchNorm(λ, β, γ, μ, σ², ϵ, momentum) BatchNorm(λ, β, γ, μ, σ², ϵ, momentum, nothing)
 @deprecate GroupNorm(G, λ, β, γ, μ, σ², ϵ, momentum) GroupNorm(G, λ, β, γ, μ, σ², ϵ, momentum, nothing)
+
+# v0.13 deprecations
+@deprecate Conv(; weight,  bias, σ=identity, kws...) Conv(weight, bias, σ; kws...) 
+@deprecate ConvTranspose(; weight, bias, σ=identity, kws...) ConvTranspose(weight, bias, σ; kws...) 
+@deprecate DepthwiseConv(; weight, bias, σ=identity, kws...) DepthwiseConv(weight, bias, σ; kws...) 
+
+
+
+
